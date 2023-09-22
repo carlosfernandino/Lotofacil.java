8e993ff89e53f2a7fdc048007dcf017a9ea6df32
carlosfernandino <142689457+carlosfernandino@users.noreply.github.com>
2023-09-22 12:41:49 -0300

    Update InterfaceLotofacil.java

--- InterfaceLotofacil.java
+++ InterfaceLotofacil.java
@@ -3,6 +3,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.util.Random;
 
 public class InterfaceLotofacil extends JFrame{
     // Atributos
@@ -12,6 +13,8 @@ public class InterfaceLotofacil extends JFrame{
     private JButton jButtonAposta3 = new JButton("Apostar par ou ímpar");
     private JLabel jLabelMensagem = new JLabel("Exemplo de Simples Janela");
     // Construtor
+    Random random = new Random();
+    int numero_sorteado = -1;
     public InterfaceLotofacil(){
         this.setTitle("Lotofácil - Interface Gráfica");
         this.setSize(400,200);
@@ -21,8 +24,24 @@ public class InterfaceLotofacil extends JFrame{
         jButtonAposta1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String aposta = JOptionPane.showInputDialog(null, "Digite um número de 0 a 100:");
-
+                try {
+                    String aposta = JOptionPane.showInputDialog(null, "Digite um número de 0 a 100:");
+                    numero_sorteado = random.nextInt(101);
+                    int numero = Integer.parseInt(aposta);
+                    if (numero < 0 || numero > 100) {
+                        JOptionPane.showMessageDialog(null, "Aposta Inválida");
+                    } else if (numero >= 0 && numero <= 100) {
+                        if (numero == numero_sorteado) {
+                            JOptionPane.showMessageDialog(null, "Parabéns! Você ganhou R$1000 reais.");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Não foi dessa vez. O número sorteado foi: " + numero_sorteado);
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Aposta Inválida");
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(null, "Entrada Inválida. Digite um número válido.");
+                }
             }
         });
 
@@ -30,13 +49,36 @@ public class InterfaceLotofacil extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 String aposta = JOptionPane.showInputDialog(null, "Digite uma letra de A a Z:");
+                char letra_premiada = 'C';
+                if (aposta != null && aposta.length() == 1 && Character.isLetter(aposta.charAt(0))) {
+                    char letra_aposta = Character.toUpperCase(aposta.charAt(0));
+
+                    if(letra_aposta == letra_premiada){
+                        JOptionPane.showMessageDialog(null,"Parabéns! Você ganhou R$500 reais.");
+                    }else{
+                        JOptionPane.showMessageDialog(null,"Não foi dessa vez. A letra premiada era:"+letra_premiada);
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(null,"Aposta Inválida.");
+                }
             }
         });
 
         jButtonAposta3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String aposta = JOptionPane.showInputDialog(null, "Escolha entre Par ou Impar:");
+                try {
+                    String aposta = JOptionPane.showInputDialog(null, "Digite um número Par ou Impar:");
+                    int par_impar = Integer.parseInt(aposta);
+
+                    if (par_impar % 2 == 0) {
+                        JOptionPane.showMessageDialog(null, "Parabéns! Você ganhou R$100 reais.");
+                    } else{
+                        JOptionPane.showMessageDialog(null, "Que pena! O número digitado é ímpar e a premiação foi para números pares.");
+                    }
+                } catch (NumberFormatException ex){
+                    JOptionPane.showMessageDialog(null, "Entrada Inválida. Digite um número válido.");
+                }
             }
         });
 
